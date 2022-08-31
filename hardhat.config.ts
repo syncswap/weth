848ef34fbc4a14dfe7236ce3071d9d50078bf7f5
nakato <94997114+0xnakato@users.noreply.github.com>
2022-08-31 17:20:11 +0000

    Update deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,7 @@ module.exports = {
     version: "latest",
     compilerSource: "binary",
     settings: {
-      compilerPath: "./zksolc-linux-amd64-musl-v1.1.3",
+      compilerPath: "./zksolc-linux-amd64-musl-v1.1.5",
     },
   },
 
